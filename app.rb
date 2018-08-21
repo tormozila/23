8e9f2362f1401a183b4da5108e55dbbe8d5589e8
Tormozila <tormozila@gmail.com>
2018-08-21 19:23:50 +0300

    list master

--- app.rb
+++ app.rb
@@ -5,15 +5,36 @@ require 'sinatra/reloader'
 
 
 get '/' do
-  erb :about
+
+  erb "12345"
+
+
 end
 
 get '/start' do
 
   @title = "Users list"
+  @logfile1 = File.open './public/users.txt', 'r'
   @logfile = File.open './public/users.txt', 'r'
   @master_arr = ["John", "Bill", "Jessie", "Martin"]
-  erb :showu
+
+  @arr = []
+  @arr1 = []
+  @value = nil
+  @value1 = nil
+
+  while (@line = @logfile.gets)
+
+    @value = @line.split ";"
+    @arr << @value[3].to_s
+  end
+  @arr.each do |line|
+
+    @value1 = line.split ":"
+    @arr1 << @value1[1].to_s
+  end
+
+ erb :showu
 
 end
 
@@ -34,5 +55,3 @@ post '/visit' do
   erb :visit
 
 end
-
-
